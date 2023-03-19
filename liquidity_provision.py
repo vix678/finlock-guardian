1467ef6ae2d7b20cea731e36909d93ce5f4d07c5
vix678 <157327328+vix678@users.noreply.github.com>
2023-03-19 14:00:01 +0200

    Update smart contract tests

--- liquidity_provision.py
+++ liquidity_provision.py
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Enhance contract modularity for future upgrades
 // Add dynamic contract upgrade mechanisms
+// Integrate Chainlink VRF for randomness
