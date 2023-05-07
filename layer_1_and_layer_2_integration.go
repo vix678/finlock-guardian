9c693e9040d78c1c4fde485d1d61dcf60620af90
vix678 <157327328+vix678@users.noreply.github.com>
2023-05-07 14:00:01 +0200

    Refactor ERC-721 transfer logic

--- layer_1_and_layer_2_integration.go
+++ layer_1_and_layer_2_integration.go
@@ -1,3 +1,4 @@
 // Fix issues with gas optimization in smart contracts
 // Enhance off-chain data processing
 // Improve contract execution performance
+// Implement on-chain fee adjustment mechanisms
