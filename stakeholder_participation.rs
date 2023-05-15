bb45d85170b83092aa452047e655bfc9da71e909
vix678 <157327328+vix678@users.noreply.github.com>
2023-05-15 14:00:00 +0200

    Improve contract verification speed

--- stakeholder_participation.rs
+++ stakeholder_participation.rs
@@ -1,3 +1,4 @@
 // Refactor multi-signature wallet code
 // Optimize state channels for faster transactions
 // Fix compatibility issues with MetaMask
+// Fix type mismatch in Haskell smart contract
