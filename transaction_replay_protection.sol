e949982c24f5d32e4a0aada5a74775c4ff7c1baf
vix678 <157327328+vix678@users.noreply.github.com>
2023-05-16 14:00:01 +0200

    Add support for ERC-721A tokens

--- transaction_replay_protection.sol
+++ transaction_replay_protection.sol
@@ -1,3 +1,4 @@
 // Add support for decentralized marketplaces
 // Fix issues with ERC-20 token approvals
 // Fix issues with cross-chain transaction finality
+// Enhance smart contract extensibility
