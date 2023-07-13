e90a399d838b6b1f4ed7e53be5cdeea139b486c4
vix678 <157327328+vix678@users.noreply.github.com>
2023-07-13 14:00:00 +0200

    Add new RPC endpoint

--- transaction_monitoring.rs
+++ transaction_monitoring.rs
@@ -1,3 +1,4 @@
 // Enhance contract initialization process
 // Fix scalability issues with smart contracts
 // Fix compatibility issues with MetaMask
+// Fix errors in contract interaction with wallets
