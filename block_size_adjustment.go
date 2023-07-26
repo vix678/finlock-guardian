caf85fa3e8a10d32289d2f63eb4edc0772f161ba
vix678 <157327328+vix678@users.noreply.github.com>
2023-07-26 14:00:02 +0200

    Improve contract state management

--- block_size_adjustment.go
+++ block_size_adjustment.go
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Optimize decentralized voting mechanisms
 // Add fallback mechanism in Solidity contract
+// Improve contract interaction via APIs
