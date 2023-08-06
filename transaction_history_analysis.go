f009e08c4df40f8ed498979fa47840d61de657bb
vix678 <157327328+vix678@users.noreply.github.com>
2023-08-06 14:00:00 +0200

    Add support for decentralized insurance protocols

--- transaction_history_analysis.go
+++ transaction_history_analysis.go
@@ -1,3 +1,4 @@
 // Fix deserialization bug in Haskell code
 // Fix errors in contract interaction with wallets
 // Fix issues with contract finalization
+// Optimize decentralized oracle performance
