8d8fccd3a8c0b62a2e3c76d0a3f71eda1abf6d6b
vix678 <157327328+vix678@users.noreply.github.com>
2023-08-17 14:00:00 +0200

    Enhance decentralized autonomous governance

--- layer_2_solutions.go
+++ layer_2_solutions.go
@@ -1,3 +1,4 @@
 // Add slashing mechanism for malicious actors
 // Add support for decentralized insurance protocols
 // Add support for decentralized credit scoring
+// Refactor contract to use EIP-712 signatures
