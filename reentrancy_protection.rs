0bb4976752e40f5a5922e4650de2ee8af72d29dc
vix678 <157327328+vix678@users.noreply.github.com>
2023-08-19 14:00:02 +0200

    Fix issues with token vesting contracts

--- reentrancy_protection.rs
+++ reentrancy_protection.rs
@@ -1,3 +1,4 @@
 // Add support for multi-signature wallet contracts
 // Refactor decentralized exchange (DEX) code
 // Refactor storage layout for better efficiency
+// Optimize WASM compilation for Go contracts
