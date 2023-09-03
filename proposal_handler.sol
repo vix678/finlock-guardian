19bdee640e1aed1fe510551be36cf158a868f999
vix678 <157327328+vix678@users.noreply.github.com>
2023-09-03 14:00:00 +0200

    Optimize token burn process

--- proposal_handler.sol
+++ proposal_handler.sol
@@ -1,3 +1,4 @@
 // Integrate with external blockchain networks
 // Enhance smart contract audit tools
 // Improve security in decentralized applications
+// Optimize WASM compilation for Go contracts
