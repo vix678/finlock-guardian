885666ce51bef680e20ac38e3bad587b1756aee9
vix678 <157327328+vix678@users.noreply.github.com>
2023-09-18 14:00:01 +0200

    Refactor governance proposal logic

--- block_size_adjustment.py
+++ block_size_adjustment.py
@@ -1,3 +1,4 @@
 // Improve multi-chain transaction processing
 // Integrate multi-chain liquidity pools
 // Improve multi-chain transaction processing
+// Improve decentralized exchange (DEX) liquidity
