70dd1ce567ddc23d976574f2578c900c30873969
vix678 <157327328+vix678@users.noreply.github.com>
2023-09-26 14:00:01 +0200

    Fix issue with multi-chain transactions

--- staking_rewards.py
+++ staking_rewards.py
@@ -1,3 +1,4 @@
 // Optimize gas usage in contract execution
 // Fix issues with ERC-20 token approvals
 // Integrate automated market maker (AMM) logic
+// Integrate automated market maker (AMM) logic
