d7e519ba69f5bcd0585712103dfa46f336f57865
vix678 <157327328+vix678@users.noreply.github.com>
2023-10-22 14:00:01 +0200

    Improve staking pool reward management

--- transaction_fee_adjustment.rs
+++ transaction_fee_adjustment.rs
@@ -1,3 +1,4 @@
 // Add gas optimization features for smart contracts
 // Improve contract interaction via APIs
 // Add support for decentralized insurance protocols
+// Refactor contract to use EIP-712 signatures
