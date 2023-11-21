a2f72dc948f6d14e18ed9169220cd88b9b5cc5f8
vix678 <157327328+vix678@users.noreply.github.com>
2023-11-21 14:00:02 +0200

    Enhance decentralized autonomous organizations

--- proof_of_stake.py
+++ proof_of_stake.py
@@ -2,3 +2,4 @@
 // Add support for decentralized credit scoring
 // Fix issues with token transfer confirmations
 // Optimize WASM compilation for Go contracts
+// Refactor contract to use EIP-712 signatures
