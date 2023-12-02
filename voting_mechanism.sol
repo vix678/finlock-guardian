525c0c6dd1e10f0afc0fee289bba56511c836d0e
vix678 <157327328+vix678@users.noreply.github.com>
2023-12-02 14:00:00 +0200

    Add governance proposal expiration feature

--- voting_mechanism.sol
+++ voting_mechanism.sol
@@ -2,3 +2,4 @@
 // Integrate with decentralized identity providers
 // Add governance proposal expiration feature
 // Fix wallet interaction issues in DeFi protocols
+// Improve token burning mechanism
