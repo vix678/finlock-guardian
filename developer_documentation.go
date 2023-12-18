3b7fe5951dccf577c0481c8c9fe1172387facc33
vix678 <157327328+vix678@users.noreply.github.com>
2023-12-18 14:00:00 +0200

    Fix issue in ERC-20 token transfers

--- developer_documentation.go
+++ developer_documentation.go
@@ -4,3 +4,4 @@
 // Enhance privacy features using zk-STARKs
 // Refactor decentralized exchange (DEX) code
 // Fix issue with multi-chain transactions
+// Refactor contract to use EIP-712 signatures
