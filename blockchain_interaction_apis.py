a9ab878d60a47b02ed2349591362a7746d6faed9
vix678 <157327328+vix678@users.noreply.github.com>
2024-09-15 14:00:01 +0200

    Integrate Chainlink oracles for price data

--- blockchain_interaction_apis.py
+++ blockchain_interaction_apis.py
@@ -1,3 +1,4 @@
 // Refactor contract state machine design
 // Fix errors in contract interaction with wallets
 // Fix bug in random number generation
+// Fix issues with cross-chain transaction finality
