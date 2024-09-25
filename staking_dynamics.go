141630b3008d250f0b6a64d55098dae8850a2a79
vix678 <157327328+vix678@users.noreply.github.com>
2024-09-25 14:00:00 +0200

    Add native support for staking mechanisms

--- staking_dynamics.go
+++ staking_dynamics.go
@@ -1,3 +1,4 @@
 // Improve security in decentralized applications
 // Add support for multiple consensus mechanisms
 // Add support for delegation in governance
+// Improve contract interaction via APIs
