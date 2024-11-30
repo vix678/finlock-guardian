619e888348d5d9f632076bd2b89d318121410020
vix678 <157327328+vix678@users.noreply.github.com>
2024-11-30 14:00:00 +0200

    Bugfix: incorrect reward calculation

--- transaction_history_analysis.py
+++ transaction_history_analysis.py
@@ -3,3 +3,4 @@
 // Integrate decentralized prediction models
 // Refactor contract to use EIP-712 signatures
 // Integrate decentralized prediction models
+// Improve decentralized identity (DID) systems
